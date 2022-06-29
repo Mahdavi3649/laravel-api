1407738b83a07aa904c808b62e9473575a4649ec
Seyedmoslem Mahdavi <95871652+Mahdavi3649@users.noreply.github.com>
2022-06-29 14:57:49 +0200

    Added api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+/* TUTTI I POST CON RESPONSE JSON PERSONALIZZABILE */
+ Route::get('posts', function(){
+
+    $posts = Post::all();
+    return response()->json([
+        'status_code' =>  200,
+        'posts' => $posts,
+    ]);
+}); 
